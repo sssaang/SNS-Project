0d25776416f8395c202f23870b3ac771dfbfc158
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-12-06 00:00:52 +0900

    connet postgre

--- api/src/main/java/com/sssaang/api/ApiApplication.java
+++ api/src/main/java/com/sssaang/api/ApiApplication.java
@@ -2,6 +2,8 @@ package com.sssaang.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -11,3 +13,4 @@ public class ApiApplication {
 	}
 
 }
+
---
+++ api/src/main/java/com/sssaang/api/HelloController.java
@@ -0,0 +1,13 @@
+package com.sssaang.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "fuck you dilan!";
+    }
+}
+
