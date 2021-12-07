4756c3e619848ecc781e35e7fb9e8b389da78caa
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-12-08 00:43:11 +0900

    feat: user repository interface

--- api/src/main/java/com/sssaang/api/repository/UserRepository.java
+++ api/src/main/java/com/sssaang/api/repository/UserRepository.java
@@ -1,4 +1,18 @@
 package com.sssaang.api.repository;
 
-public class UserRepository {
+import com.sssaang.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    List<User> findByIdIn(List<Long> userIds);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
