30a4fb2b24fbd44ff3d1482cb304798c89551fcd
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-12-07 23:55:29 +0900

    feat; data audit for update and creation date

--- api/src/main/java/com/sssaang/api/entity/User.java
+++ api/src/main/java/com/sssaang/api/entity/User.java
@@ -1,20 +1,19 @@
-package com.sssaang.api.model;
+package com.sssaang.api.entity;
 
+import com.sssaang.api.entity.audit.DateAudit;
 import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = { "username" }),
         @UniqueConstraint(columnNames = { "email" })
 })
-public class User {
+public class User extends DateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
---
+++ api/src/main/java/com/sssaang/api/entity/audit/DateAudit.java
@@ -0,0 +1,28 @@
+package com.sssaang.api.entity.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = { "createdAt", "updatedAt" },
+        allowGetters = true
+)
+public abstract class DateAudit implements Serializable {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    public Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    public Instant updatedAt;
+}
