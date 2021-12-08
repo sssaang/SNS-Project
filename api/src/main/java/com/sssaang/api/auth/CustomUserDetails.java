d804e9639b5e1844563558f86779e628bb3b0aa3
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-12-08 23:39:30 +0900

    feat: user detail for authentication

--- api/src/main/java/com/sssaang/api/auth/CustomUserDetails.java
+++ api/src/main/java/com/sssaang/api/auth/CustomUserDetails.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class CustomUserDetails implements UserDetails {
@@ -17,31 +18,64 @@ public class CustomUserDetails implements UserDetails {
 
     @JsonIgnore public String email;
     @JsonIgnore private String password;
-    private GrantedAuthority authority;
 
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
+    private Collection<? extends GrantedAuthority> authorities;
 
     public CustomUserDetails(Long id, String name, String username, String email, String password,
-                             GrantedAuthority authority) {
+                             Collection<? extends GrantedAuthority> authorities) {
         this.id = id;
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
-        this.authority = authority;
+        this.authorities = authorities;
     }
 
     public static CustomUserDetails create(User user) {
         GrantedAuthority authority = new  new SimpleGrantedAuthority(user.name);
         return new CustomUserDetails(user.id, user.name, user.username, user.email, user.password, authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override public boolean isEnabled() {
+        return true;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CustomUserDetails that = (CustomUserDetails) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(id);
     }
 }
